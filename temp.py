782c3dc92ad756ee48a76ac542e1e6feffd513d1
ch3p4ll3 <slinucs@gmail.com>
2020-05-28 19:48:33 +0200

    added service, main program, updated README.md

--- temp.py
+++ temp.py
@@ -1,25 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import RPi.GPIO as gpio
+import pigpio
 import time
 import psutil
 
-gpio.setwarnings(False)
-gpio.setmode(gpio.BCM)
-pin = 13
-gpio.setup(pin, gpio.OUT)
-fan = gpio.PWM(pin, 50)
-fan.start(0)
 
-out_min = 0    # min speed
-in_min = 25    # temperature min
-out_max = 100  # max speed
-in_max = 65    # temperature max
+GPIO = 13  # GPIO pin (BCM) of the transistor that controls the fan
+
+pi = pigpio.pi()
+
+if not pi.connected:
+    exit(0)
+
+pi.set_mode(GPIO, pigpio.OUTPUT)
+pi.set_PWM_frequency(GPIO, 20000)  # Set frequency at 20KHz
+pi.set_PWM_dutycycle(GPIO, 0)
+
+out_min = 77  # min speed -> 77 = 30%
+out_max = 255  # max speed -> 255 = 100%
+in_min = 25  # temperature min
+in_max = 65  # temperature max
 
 while True:
-	tempC = psutil.sensors_temperatures()['cpu-thermal'][0][1]
-	calc = (tempC - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-	if calc > 100:
-		calc = 100
-	fan.ChangeDutyCycle(calc)
-	time.sleep(0.5)
+    try:
+        tempC = psutil.sensors_temperatures()['cpu-thermal'][0][1]
+        calc = (tempC - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+        if calc > 255:
+            calc = 255
+
+        elif calc < 77:
+            calc = 77
+
+        print(calc, " - ", tempC)
+
+        pi.set_PWM_dutycycle(GPIO, calc)
+
+        time.sleep(.5)
+
+    except KeyboardInterrupt:
+        pi.stop()
